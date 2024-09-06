79aa402faefa4ad60e0a0e7ddadad36b086d83c6
alexandre.lsantos19 <alexandre.lsantos19@VPR0687150W10-1>
2024-09-05 21:23:12 -0300

    Novo Comentario

--- Aula06_UC11_Teste02/Form1.cs
+++ Aula06_UC11_Teste02/Form1.cs
@@ -3,6 +3,7 @@ namespace Aula06_UC11_Teste02
     public partial class Form1 : Form
     {
         //Construtor  da classe Form1
+        // Comentário 2
         public Form1()
         {
             InitializeComponent();
